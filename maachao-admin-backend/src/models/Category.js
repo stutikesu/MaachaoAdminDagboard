4094c7f954089b57064cbbf59414dcdcb4a1d318
Stuti Kesarwani <115646191+stutikesu@users.noreply.github.com>
2025-09-01 23:14:18 +0530

    Update Category.js

--- maachao-admin-backend/src/models/Category.js
+++ maachao-admin-backend/src/models/Category.js
@@ -1,13 +1,1 @@
-import mongoose from "mongoose";
 
-const CategorySchema = new mongoose.Schema({
-  name: { type: String, required: true, trim: true },
-  slug: { type: String, required: true, unique: true },
-  description: String,
-  parent: { type: mongoose.Schema.Types.ObjectId, ref: "Category", default: null }, // parent -> subcategory
-  imageUrl: String,
-  isDeleted: { type: Boolean, default: false },
-  deletedAt: Date,
-}, { timestamps: true });
-
-export default mongoose.model("Category", CategorySchema);
